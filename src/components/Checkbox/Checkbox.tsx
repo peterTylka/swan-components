30dbe85cedae4a85809edf2fb6f7350ecf418d43
Peter Tylka <ptylka@gmail.com>
2026-06-12 10:24:11 +0200

    fix(Checkbox): use Figma check icon

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -18,13 +18,13 @@ export interface CheckboxProps extends Omit<
 
 function CheckIcon() {
   return (
-    <svg viewBox="0 0 16 16" fill="none" className="h-4 w-4" aria-hidden="true">
+    <svg width="12" height="9" viewBox="0 0 12 9" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
-        d="M3.5 8.5l3 3 6-6"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
+        d="M11 1L4.125 8L1 4.81818"
+        stroke="white"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
       />
     </svg>
   )
